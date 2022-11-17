6ca2c962e922af4c14cb7d30212e98fd8989c373
Naman Gera <43800580+naman1999@users.noreply.github.com>
2022-11-17 13:36:17 -0500

    Added downstream function

--- src/queries/cachement.js
+++ src/queries/cachement.js
@@ -3,13 +3,53 @@
 import axios from 'axios';
 export async function getCachement(long, lat, projection){console.log(long + " " + lat + " " + projection)
 axios.get('http://localhost:3000/users/neo2/'+long+'/'+lat+'/'+projection
-// axios.get('http://localhost:3000/users/ne'
-// {
-//   params: {
-//   long: long
-//   // lat: lat,
-//   // projection: projection
-// }}
+
+)   
+  .then(function (response) {
+    console.log(response);
+    const mapId = 'mapWM';
+    cgpv.api.map(mapId).layer.vector?.createGeometryGroup('myGometryGroup');
+    const defaultId = cgpv.api.map(mapId).layer.vector?.defaultGeometryGroupId;
+    if (response.data[0].row_to_json.features[0].geometry.type=='Polygon'){
+        const geom = cgpv.api.map(mapId).layer.vector?.addPolygon(
+          response.data[0].row_to_json.features[0].geometry.coordinates,{
+            style: {
+              strokeColor: '#3d85c6',
+              strokeWidth: 5,
+              strokeOpacity: 1,
+            },
+          }
+        );
+        cgpv.api.map(mapId).layer.vector?.addToGeometryGroup(
+          geom, 'myGometryGroup'
+        );
+        cgpv.api.map(mapId).layer.vector?.deleteGeometriesFromGroup(defaultId);
+    }
+    else{
+    for (const element of response.data[0].row_to_json.features[0].geometry.coordinates){
+      const geom = cgpv.api.map(mapId).layer.vector?.addPolygon(
+        element,{
+          style: {
+            strokeColor: '#3d85c6',
+            strokeWidth: 5,
+            strokeOpacity: 1,
+          },
+        }
+      );
+      cgpv.api.map(mapId).layer.vector?.addToGeometryGroup(
+        geom, 'myGometryGroup'
+      );
+      cgpv.api.map(mapId).layer.vector?.deleteGeometriesFromGroup(defaultId);
+    };
+    }
+  })
+  .catch(function (error) {     
+     console.log(error);   })   
+}
+
+
+export async function getDownstream(long, lat, projection){console.log(long + " " + lat + " " + projection)
+axios.get('http://localhost:3000/users/downstream/'+long+'/'+lat+'/'+projection
 )   
   .then(function (response) {
     console.log(response);
