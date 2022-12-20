dda2339dc1b65d0183dbde88d2ca5d5e6fde5063
Naman Gera <43800580+naman1999@users.noreply.github.com>
2022-12-20 13:48:51 -0500

    update queries.js
    removed useless code

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,24 +1,24 @@
 
-const { Pool, Client } = require('pg')
-const pool = new Pool({
-  user: 'naman',
-  host: 'localhost',
-  database: 'NHD',
-  password: 'sherbrooke',
-  port: 5432,
-})
+// const { Pool, Client } = require('pg')
+// const pool = new Pool({
+//   user: 'naman',
+//   host: 'localhost',
+//   database: 'NHD',
+//   password: 'sherbrooke',
+//   port: 5432,
+// })
 
-const client = new Client({
-  user: 'naman',
-  host: 'localhost',
-  database: 'NHD',
-  password: 'sherbrooke',
-  port: 5432,
-})
-client.connect()
+// const client = new Client({
+//   user: 'naman',
+//   host: 'localhost',
+//   database: 'NHD',
+//   password: 'sherbrooke',
+//   port: 5432,
+// })
+// client.connect()
 
-module.exports = {
-  query: (text, params) => 
-     pool.query(text, params),
-}
+// module.exports = {
+//   query: (text, params) => 
+//      pool.query(text, params),
+// }
 
